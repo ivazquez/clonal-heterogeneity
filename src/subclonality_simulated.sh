f4a9c30b566e5296025ab11e703b0475eee2a69e
Ignacio Vázquez García <ivg@sanger.ac.uk>
2016-04-26 14:06:15 +0200

    cloneHD example with real data

--- src/subclonality_simulated.sh
+++ src/subclonality_simulated.sh
@@ -5,7 +5,7 @@
 # set the number of threads
 export OMP_NUM_THREADS=4;
 
-# change this to your population-dynamics directory, or remove if
+# change this to your gv-paper-2016 directory, or remove if
 # you put filterHD and cloneHD into your system-wide path:
 CWD=$(git rev-parse --show-toplevel)
 BUILD_DIR=${CWD}/build
@@ -15,7 +15,12 @@ data=${CWD}/data/seq/subclonality/simulation
 results=${CWD}/data/seq/subclonality/simulation
 
 bulkSNV="${data}/bulk.snv.txt"
+mixtureCNA="${data}/mixture.cna.txt"
 mixtureSNV="${data}/mixture.snv.txt"
+mixtureSNVjumps="${results}/mixture.snv.jumps.txt"
+bulkProfile="${results}/bulk.snv.posterior-1.txt"
+pre="${results}/mixture"
+clones="${data}/clones.txt"
 
 
 ### filterHD ###
@@ -29,3 +34,14 @@ $cmd
 cmd="${BUILD_DIR}/filterHD --data $mixtureSNV --mode 1 --pre ${results}/mixture.snv --rnd 1e-8 --jumps 1"
 echo $cmd
 $cmd
+
+
+### cloneHD ###
+echo "*** cloneHD ***"
+echo "True mass and cell fractions:" `cat $clones` 
+   
+cmd="${BUILD_DIR}/cloneHD --cna $mixtureCNA --snv $mixtureSNV --pre $pre --trials 5 --nmax 3 --force --max-tcn 2 --cna-jump 0 --snv-jumps $mixtureSNVjumps --min-jump 0.01 --restarts 10 --mass-gauging 1 --bulk-mean $bulkProfile --cna-pen-norm 0.9" 
+echo $cmd
+$cmd
+echo
+cat ${pre}.summary.txt
