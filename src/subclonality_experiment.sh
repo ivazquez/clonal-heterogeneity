14ddc4296e53a04a79a07abe10652467cf97ce9c
Ignacio Vázquez García <ivg@sanger.ac.uk>
2016-05-07 19:41:06 +0200

    SNV allele counts for the cloneHD example with real data

--- src/subclonality_experiment.sh
+++ src/subclonality_experiment.sh
@@ -11,11 +11,11 @@ CWD=$(git rev-parse --show-toplevel)
 BUILD_DIR=${CWD}/build
 
 # input and output directories
-data=${CWD}/data/seq/subclonality/experiment
-results=${CWD}/data/seq/subclonality/experiment
+data=${CWD}/data/seq/subclonality/experiment/WAxNA_F12_1_RM_1
+results=${CWD}/data/seq/subclonality/experiment/WAxNA_F12_1_RM_1
 
-bulkSNV="${data}/bulk.snv.txt"
-mixtureSNV="${data}/mixture.snv.txt"
+bulkSNV="${data}/snv_T0.txt"
+mixtureSNV="${data}/snv_T2_T32.txt"
 mixtureSNVjumps="${results}/mixture.snv.jumps.txt"
 bulkProfile="${results}/bulk.snv.posterior-1.txt"
 pre="${results}/mixture"
@@ -26,6 +26,10 @@ clones="${data}/clones.txt"
 echo "*** filterHD ***"
 echo
 
+cmd="${BUILD_DIR}/filterHD --data $bulkSNV --mode 1 --pre ${results}/bulk.snv --rnd 1e-8 --jump 0 --dist 1"
+echo $cmd
+$cmd
+
 cmd="${BUILD_DIR}/filterHD --data $mixtureSNV --mode 1 --pre ${results}/mixture.snv --rnd 1e-8 --jumpi 1e-08 --sigmai 1.0e-4 --jumps 1"
 echo $cmd
 $cmd
@@ -35,8 +39,7 @@ $cmd
 echo "*** cloneHD ***"
 echo "True mass and cell fractions:" `cat $clones` 
    
-cmd="${BUILD_DIR}/cloneHD --snv $mixtureSNV --pre $pre --trials 5 --nmax 3 --force --snv-rnd 1.0e-4 --snv-jump 0.001 --bulk-mean $bulkProfile" 
-
+cmd="${BUILD_DIR}/cloneHD --snv $mixtureSNV --pre $pre --trials 5 --nmax 3 --force --snv-rnd 1.0e-4 --snv-jump 0.001 --bulk-mean $bulkProfile"
 echo $cmd
 $cmd
 echo
